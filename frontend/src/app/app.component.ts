908a6594c44e9838d0cc0e7830f706567d919905
Andrew Lockard <alockard@unc.edu>
2024-09-06 12:40:45 -0400

    Remove scss

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -11,7 +11,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   tas = ["Andrew", "Ajay", "Jade", "Will", "Audrey", "Kiernan", "Ellie"];
